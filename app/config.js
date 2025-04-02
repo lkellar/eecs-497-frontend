fca1961fcfdf8330ad7c27f7e9f6f2587e2bfb1e
Lucas K <lucas@lkellar.org>
2025-04-02 18:54:16 -0400

    update name

--- app/config.js
+++ app/config.js
@@ -1,5 +1,4 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-// currently tbd, so just putting it here so we can change it later
-const PRODUCT_NAME = '497 Language Project'
+const PRODUCT_NAME = 'SoleSpeaks';
 
 export {API_BASE_URL, PRODUCT_NAME};
